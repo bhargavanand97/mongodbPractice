3f47d1ee75641029dc7141a171081d5f61eacb5c
Bhargav Anand <bhargavanand97@gmail.com>
2020-03-06 00:40:34 +0530

    After adding mocha and running a dummy test script

---
+++ test/demo_test.js
@@ -0,0 +1,11 @@
+const mocha = require('mocha');
+const assert = require('assert');
+
+// Describe the tests
+
+describe('Some Demo Tests',function(){
+    // Creating tests
+    it('Adding two numbers',function(){
+        assert(2+3 === 5);
+    })
+})
